60fc698f3e24a07f11384a2fbe65f95cdeef570c
DESKTOP-6OK9LFJ\Danish <danish.hassan@citrusbits.com>
2019-04-10 15:10:16 +0500

    test commit 1

---
+++ app/src/main/java/com/example/emp/expandablelistviewexample/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.emp.expandablelistviewexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import com.example.emp.expandablelistviewexample.adapter.ExpListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ExpandableListView listView;
+    private ExpListAdapter listAdapter;
+    private List<String> listDataHeader;
+    private HashMap<String,List<String>> listHash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ExpandableListView) findViewById(R.id.lvExp);
+        initData();
+        listAdapter = new ExpListAdapter(this,listDataHeader,listHash);
+        listView.setAdapter(listAdapter);
+
+
+        /**
+         * Select Only One Item at a time
+         */
+        listView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+
+                int len = listAdapter.getGroupCount();
+
+                for (int i = 0; i < len; i++) {
+                    if (i != groupPosition) {
+                        listView.collapseGroup(i);
+                    }
+                }
+
+            }
+        });
+    }
+
+
+    private void initData() {
+        listDataHeader = new ArrayList<>();
+        listHash = new HashMap<>();
+
+        listDataHeader.add("Windows");
+        listDataHeader.add("Android");
+        listDataHeader.add("MAC");
+
+        List<String> windows = new ArrayList<>();
+        windows.add("Windows 7");
+        windows.add("Windows 8");
+        windows.add("Windows 10");
+
+        List<String> android = new ArrayList<>();
+        android.add("Android 4");
+        android.add("Android 5");
+        android.add("Android 6");
+
+        List<String> mac = new ArrayList<>();
+        mac.add("Mac 8");
+        mac.add("Mac 9");
+        mac.add("Mac 10");
+
+        listHash.put(listDataHeader.get(0),windows);
+        listHash.put(listDataHeader.get(1),android);
+        listHash.put(listDataHeader.get(2),mac);
+
+    }
+}
---
+++ app/src/main/java/com/example/emp/expandablelistviewexample/adapter/ExpListAdapter.java
@@ -0,0 +1,100 @@
+package com.example.emp.expandablelistviewexample.adapter;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import com.example.emp.expandablelistviewexample.R;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by emp on 11/21/2017.
+ */
+
+public class ExpListAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private List<String> listDataHeader;
+    private HashMap<String,List<String>> listHashMap;
+
+    public ExpListAdapter(Context context, List<String> listDataHeader, HashMap<String,List<String>> listHashMap) {
+        this.context = context;
+        this.listDataHeader = listDataHeader;
+        this.listHashMap = listHashMap;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listDataHeader.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return listHashMap.get(listDataHeader.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return listDataHeader.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return listHashMap.get(listDataHeader.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        if(convertView == null)
+        {
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_group,null);
+        }
+        TextView lblListHeader = (TextView) convertView.findViewById(R.id.lblListHeader);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final String childText = (String)getChild(groupPosition,childPosition);
+        if(convertView == null)
+        {
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item,null);
+        }
+        TextView lblListItem = (TextView) convertView.findViewById(R.id.lblListItems);
+        lblListItem.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+}
