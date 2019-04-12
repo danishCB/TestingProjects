db290a17969b137ac2c85fb05d3445d6259f14f8
DESKTOP-6OK9LFJ\Danish <danish.hassan@citrusbits.com>
2019-04-12 15:47:27 +0500

    Changes1

--- app/src/main/java/com/example/emp/expandablelistviewexample/MainActivity.java
+++ app/src/main/java/com/example/emp/expandablelistviewexample/MainActivity.java
@@ -52,9 +52,9 @@ public class MainActivity extends AppCompatActivity {
         listDataHeader = new ArrayList<>();
         listHash = new HashMap<>();
 
-        listDataHeader.add("Windows1");
-        listDataHeader.add("AndroidPie");
-        listDataHeader.add("MAC123");
+        listDataHeader.add("Windows11");
+        listDataHeader.add("AndroidQ");
+        listDataHeader.add("MAC10");
 
         List<String> windows = new ArrayList<>();
         windows.add("Windows 8");
